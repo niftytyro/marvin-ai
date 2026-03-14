6bbcd05616f86297e8746c930725196369b61490
Udasi Tharani <udasitharani@gmail.com>
2026-03-14 14:37:18 +0530

    migrate from View to Box

--- components/Marvin.tsx
+++ components/Marvin.tsx
@@ -1,26 +1,13 @@
 import useMicState from "@/hooks/useMic";
-import {
-  AppState,
-  Linking,
-  Pressable,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { AppState, Linking, Pressable, StyleSheet } from "react-native";
 import Button from "./Button";
 import { useEffect } from "react";
 import useConversation from "@/hooks/useConversation";
 import { ConversationStatus } from "@/utils/types";
 import { useNetworkState } from "expo-network";
+import { Box, Text } from "./foundation";
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-  },
   face: {
     borderRadius: 100,
     width: 200,
@@ -36,6 +23,8 @@ const Marvin: React.FC = () => {
     pauseConversation,
     resumeConversation,
     startNewConversation,
+    mute,
+    unmute,
   } = useConversation();
   const { isInternetReachable } = useNetworkState();
 
@@ -57,9 +46,15 @@ const Marvin: React.FC = () => {
 
   if (permissionStatus !== "granted") {
     return (
-      <View style={{ flex: 1 }}>
-        <View style={styles.container}>
-          <Text style={{ color: "red", fontSize: 18, textAlign: "center" }}>
+      <Box flex={1}>
+        <Box
+          flex={1}
+          width="100%"
+          height="100%"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text>
             Microphone permission is required to start the conversation.
           </Text>
 
@@ -73,20 +68,26 @@ const Marvin: React.FC = () => {
                   }
             }
           />
-        </View>
+        </Box>
 
         {!isInternetReachable && (
           <Text>Oops! It looks like you have a bad internet connection.</Text>
         )}
-      </View>
+      </Box>
     );
   }
 
   return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.container}>
+    <Box flex={1}>
+      <Box
+        flex={1}
+        width="100%"
+        height="100%"
+        justifyContent="center"
+        alignItems="center"
+      >
         <Pressable onPress={startNewConversation}>
-          <View
+          <Box
             style={[
               styles.face,
               {
@@ -100,6 +101,14 @@ const Marvin: React.FC = () => {
           />
         </Pressable>
 
+        {conversationStatus === ConversationStatus.UNINITIALIZED ? (
+          <Text>
+            {
+              "This is Marvin the Paranoid Android. Say hi... but don't talk to him about life."
+            }
+          </Text>
+        ) : null}
+
         {conversationStatus === ConversationStatus.CONNECTED ? (
           <>
             <Button label="Pause conversation" onPress={pauseConversation} />
@@ -112,7 +121,7 @@ const Marvin: React.FC = () => {
             <Button label="Resume conversation" onPress={resumeConversation} />
           </>
         ) : null}
-      </View>
+      </Box>
 
       {!isInternetReachable && (
         <Text
@@ -125,7 +134,7 @@ const Marvin: React.FC = () => {
           Oops! It looks like you have a bad internet connection.
         </Text>
       )}
-    </View>
+    </Box>
   );
 };
 

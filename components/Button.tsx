d11a13b0b9307d9ddf830c3d6392cacf4529c13e
Udasi Tharani <udasitharani@gmail.com>
2026-03-15 02:18:52 +0530

    updated button to accept more props

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,23 +2,46 @@ import React from "react";
 import { Pressable } from "react-native";
 import { Box, Text } from "./foundation";
 
-interface ButtonProps {
+export interface ButtonProps {
   label: string;
+  variant?: "primary" | "secondary" | "critical";
+  state?: "enabled" | "disabled" | "loading";
   onPress: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onPress }) => {
+const Button: React.FC<ButtonProps> = ({
+  label,
+  onPress,
+  state = "enabled",
+  variant = "primary",
+}) => {
   return (
-    <Pressable onPress={onPress}>
+    <Pressable onPress={onPress} style={{ flex: 1 }}>
       <Box
         height={48}
         px="m"
-        backgroundColor="black"
+        backgroundColor={
+          variant === "primary"
+            ? "black"
+            : variant === "secondary"
+            ? "lightGray"
+            : "lightRed"
+        }
         borderRadius={8}
         justifyContent="center"
         alignItems="center"
       >
-        <Text variant="button" color="white">
+        <Text
+          variant="button"
+          textAlign="center"
+          color={
+            variant === "primary"
+              ? "white"
+              : variant === "secondary"
+              ? "black"
+              : "darkRed"
+          }
+        >
           {label}
         </Text>
       </Box>

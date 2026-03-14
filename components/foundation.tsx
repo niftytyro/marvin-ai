8b68ee1152f119ea5f2c8c86e250a0476f12fde2
Udasi Tharani <udasitharani@gmail.com>
2026-03-15 02:18:34 +0530

    add safe top and bottom space

--- components/foundation.tsx
+++ components/foundation.tsx
@@ -1,7 +1,18 @@
 import { createBox, createText } from "@shopify/restyle";
 import { Theme } from "./theme";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-const Box = createBox<Theme>();
-const Text = createText<Theme>();
+export const Box = createBox<Theme>();
+export const Text = createText<Theme>();
 
-export { Box, Text };
+export const SafeTopSpace = () => {
+  const { top } = useSafeAreaInsets();
+
+  return <Box height={top} />;
+};
+
+export const SafeBottomSpace = () => {
+  const { bottom } = useSafeAreaInsets();
+
+  return <Box height={bottom} />;
+};

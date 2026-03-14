ab8170f09929651e139429c4ef03c30af8686519
Udasi Tharani <udasitharani@gmail.com>
2026-03-14 13:32:51 +0530

    handle no internet

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,5 @@
 import { ElevenLabsProvider } from "@elevenlabs/react-native";
 import Marvin from "@/components/Marvin";
-import { Stack } from "expo-router";
 
 export default function Index() {
   return (
--- components/Marvin.tsx
+++ components/Marvin.tsx
@@ -11,6 +11,7 @@ import Button from "./Button";
 import { useEffect } from "react";
 import useConversation from "@/hooks/useConversation";
 import { ConversationStatus } from "@/utils/types";
+import { useNetworkState } from "expo-network";
 
 const styles = StyleSheet.create({
   container: {
@@ -36,6 +37,7 @@ const Marvin: React.FC = () => {
     resumeConversation,
     startNewConversation,
   } = useConversation();
+  const { isInternetReachable } = useNetworkState();
 
   useEffect(() => {
     AppState.addEventListener("change", (state) => {
@@ -55,54 +57,74 @@ const Marvin: React.FC = () => {
 
   if (permissionStatus !== "granted") {
     return (
-      <View style={styles.container}>
-        <Text style={{ color: "red", fontSize: 18, textAlign: "center" }}>
-          Microphone permission is required to start the conversation.
-        </Text>
+      <View style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <Text style={{ color: "red", fontSize: 18, textAlign: "center" }}>
+            Microphone permission is required to start the conversation.
+          </Text>
 
-        <Button
-          label="Grant permission"
-          onPress={
-            canAskAgain
-              ? requestMicPermission
-              : () => {
-                  Linking.openURL("app-settings:");
-                }
-          }
-        />
+          <Button
+            label="Grant permission"
+            onPress={
+              canAskAgain
+                ? requestMicPermission
+                : () => {
+                    Linking.openURL("app-settings:");
+                  }
+            }
+          />
+        </View>
+
+        {!isInternetReachable && (
+          <Text>Oops! It looks like you have a bad internet connection.</Text>
+        )}
       </View>
     );
   }
 
   return (
-    <View style={[styles.container, {}]}>
-      <Pressable onPress={startNewConversation}>
-        <View
-          style={[
-            styles.face,
-            {
-              backgroundColor:
-                conversationStatus === ConversationStatus.CONNECTED
-                  ? "#d86882"
-                  : "#999",
-              marginBottom: 24,
-            },
-          ]}
-        />
-      </Pressable>
+    <View style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Pressable onPress={startNewConversation}>
+          <View
+            style={[
+              styles.face,
+              {
+                backgroundColor:
+                  conversationStatus === ConversationStatus.CONNECTED
+                    ? "#d86882"
+                    : "#999",
+                marginBottom: 24,
+              },
+            ]}
+          />
+        </Pressable>
 
-      {conversationStatus === ConversationStatus.CONNECTED ? (
-        <>
-          <Button label="Pause conversation" onPress={pauseConversation} />
-          <Button label="End conversation" onPress={endConversation} />
-        </>
-      ) : null}
+        {conversationStatus === ConversationStatus.CONNECTED ? (
+          <>
+            <Button label="Pause conversation" onPress={pauseConversation} />
+            <Button label="End conversation" onPress={endConversation} />
+          </>
+        ) : null}
 
-      {conversationStatus === ConversationStatus.DISRUPTED ? (
-        <>
-          <Button label="Resume conversation" onPress={resumeConversation} />
-        </>
-      ) : null}
+        {conversationStatus === ConversationStatus.DISRUPTED ? (
+          <>
+            <Button label="Resume conversation" onPress={resumeConversation} />
+          </>
+        ) : null}
+      </View>
+
+      {!isInternetReachable && (
+        <Text
+          style={{
+            marginBottom: 24,
+            marginHorizontal: 20,
+            textAlign: "center",
+          }}
+        >
+          Oops! It looks like you have a bad internet connection.
+        </Text>
+      )}
     </View>
   );
 };

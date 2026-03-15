25bd3f154ba9d8dc018a7c8f54c79d56242cc840
Udasi Tharani <udasitharani@gmail.com>
2026-03-15 14:40:47 +0530

    play sound on connect/disconnect

--- components/Marvin.tsx
+++ components/Marvin.tsx
@@ -8,6 +8,9 @@ import { useNetworkState } from "expo-network";
 import { Box, SafeBottomSpace, SafeTopSpace, Text } from "./foundation";
 import Face from "./Face";
 import Footer, { FooterProps } from "./Footer";
+import { useAudioPlayer } from "expo-audio";
+
+const audioSource = require("@/assets/sounds/toggle.wav");
 
 const Marvin: React.FC = () => {
   const { permissionStatus, canAskAgain, requestMicPermission } = useMicState();
@@ -21,6 +24,7 @@ const Marvin: React.FC = () => {
     unmute,
   } = useConversation();
   const { isInternetReachable } = useNetworkState();
+  const player = useAudioPlayer(audioSource);
 
   useEffect(() => {
     AppState.addEventListener("change", (state) => {
@@ -56,6 +60,18 @@ const Marvin: React.FC = () => {
     permissionStatus,
     startNewConversation,
   ]);
+
+  useEffect(() => {
+    if (conversationStatus === ConversationStatus.CONNECTING) {
+      player.play();
+    } else if (
+      conversationStatus === ConversationStatus.DISRUPTED ||
+      conversationStatus === ConversationStatus.ENDED ||
+      conversationStatus === ConversationStatus.PAUSED
+    ) {
+      player.play();
+    }
+  }, [conversationStatus, player]);
 
   const getPrimaryCta: () => ButtonProps | undefined = () => {
     if (permissionStatus !== "granted") {

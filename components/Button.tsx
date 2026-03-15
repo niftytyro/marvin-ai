0604f9f3bef59822770b1493b3d448c44312c536
Udasi Tharani <udasitharani@gmail.com>
2026-03-15 23:22:35 +0530

    add loading state in button

--- components/Button.tsx
+++ components/Button.tsx
@@ -12,6 +12,7 @@ import Animated, {
 } from "react-native-reanimated";
 import { Theme } from "./theme";
 import { LinearEasing } from "react-native-reanimated/lib/typescript/css/easing";
+import LottieView from "lottie-react-native";
 
 export interface ButtonProps {
   label: string;
@@ -38,7 +39,17 @@ const Button: React.FC<ButtonProps> = ({
       : "lightRed";
   }, [state, variant]);
 
-  // return null;
+  const textColor: keyof Theme["colors"] = useMemo(() => {
+    if (state === "disabled") {
+      return "darkGray";
+    }
+
+    return variant === "primary"
+      ? "white"
+      : variant === "secondary"
+      ? "black"
+      : "darkRed";
+  }, [state, variant]);
 
   return (
     <Animated.View
@@ -46,7 +57,11 @@ const Button: React.FC<ButtonProps> = ({
       entering={FadeInDown.duration(100).delay(200).easing(Easing.linear)}
       exiting={FadeOutDown.duration(200)}
     >
-      <Pressable onPress={onPress} style={{ flex: 1 }}>
+      <Pressable
+        onPress={onPress}
+        disabled={state !== "enabled"}
+        style={{ flex: 1 }}
+      >
         <Box
           height={48}
           px="m"
@@ -54,20 +69,23 @@ const Button: React.FC<ButtonProps> = ({
           borderRadius={"large"}
           justifyContent="center"
           alignItems="center"
+          flexDirection="row"
         >
-          <Text
-            variant="button"
-            textAlign="center"
-            color={
-              variant === "primary"
-                ? "white"
-                : variant === "secondary"
-                ? "black"
-                : "darkRed"
-            }
-          >
+          <Text variant="button" textAlign="center" color={textColor}>
             {label}
           </Text>
+          {state === "loading" && (
+            <LottieView
+              autoPlay
+              loop
+              source={
+                variant === "critical"
+                  ? require("@/assets/lotties/loading-error.json")
+                  : require("@/assets/lotties/loading.json")
+              }
+              style={{ height: 24, width: 24, marginLeft: 8 }}
+            />
+          )}
         </Box>
       </Pressable>
     </Animated.View>
